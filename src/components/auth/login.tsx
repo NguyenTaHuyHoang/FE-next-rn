6e189659d5fa9b350286a1d84084988850faab19
Nguyễn Tạ Huy Hoàng <85854007+NguyenTaHuyHoang@users.noreply.github.com>
2024-11-12 17:06:15 +0700

    forgot password

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -7,11 +7,13 @@ import { authenticate } from "@/utils/actions";
 import { useRouter } from "next/navigation";
 import ModalReactive from "./modal.reactive";
 import { useState } from "react";
+import ModalChangePassword from "./modal.changePassword";
 
 const Login = () => {
   const router = useRouter();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [userEmail, setUserEmail] = useState("");
+  const [changePassword, setChangePassword] = useState(false);
 
   const onFinish = async (values: any) => {
     const { username, password } = values;
@@ -85,9 +87,20 @@ const Login = () => {
               </Form.Item>
 
               <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Login
-                </Button>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Button type="primary" htmlType="submit">
+                    Login
+                  </Button>
+                  <Button type="link" onClick={() => setChangePassword(true)}>
+                    Quên mật khẩu ?
+                  </Button>
+                </div>
               </Form.Item>
             </Form>
             <Link href={"/"}>
@@ -105,6 +118,10 @@ const Login = () => {
         isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen}
         userEmail={userEmail}
+      />
+      <ModalChangePassword
+        isModalOpen={changePassword}
+        setIsModalOpen={setChangePassword}
       />
     </>
   );
